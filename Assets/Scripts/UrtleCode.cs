b0682068528a99586d9151ff16715708749e3f33
Anna Kasagawa <anna.kasagawa@gmail.com>
2016-09-19 14:31:08 +0200

    animation not working

--- Assets/Scripts/UrtleCode.cs
+++ Assets/Scripts/UrtleCode.cs
@@ -6,13 +6,15 @@ public class UrtleCode : MonoBehaviour {
 	public float maxSpeed = 11f;
 	bool right = true; //for flipping
 	bool ground = false;
+	bool climb = false;
 	public Transform groundChk;
 	float groundRds = 0.2f;
 	public float jumpForce = 500f;
 	public LayerMask isGround;
 
 
-	Animator a;
+	private Animator a;
+	private Rigidbody2D r;
 
 
 	// Use this for initialization
@@ -20,27 +22,47 @@ public class UrtleCode : MonoBehaviour {
 		a = GetComponent<Animator> ();
 	}
 
-	void Update () {
-		if (ground && Input.GetKeyDown (KeyCode.Space)) {
-			a.SetBool("Ground", false);
-				GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.CompareTag ("Ladder") && Input.GetKeyDown (KeyCode.UpArrow)) {
+			climb = true;
 		}
 
+		if(other.CompareTag("Puzzle Piece")){
+			Destroy(other.gameObject);
+			//GameManager.AddPoints (1);
+			//if (other.CompareTag ("Ladder")) {
+			//		}
+		}
 	}
 
 
 	// Update is called once per frame
+	void Update () {
+		if (ground && Input.GetKeyDown (KeyCode.UpArrow)) {
+			a.SetBool("Ground", false);
+				GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+		}
+	}
+		
 	void FixedUpdate () {
 
 		ground = Physics2D.OverlapCircle (groundChk.position, groundRds, isGround);
 		a.SetBool ("Ground", ground);
 		a.SetFloat ("Vertical Speed", GetComponent<Rigidbody2D> ().velocity.y);
+		a.SetBool ("Climb", climb);
+
 
 		float move = Input.GetAxis ("Horizontal");
 		float velY = GetComponent<Rigidbody2D> ().velocity.y;
 
 		a.SetFloat("speed", Mathf.Abs(move));
 		GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, velY);
+
+//		if (climb) {
+//			a.SetTrigger ("Climbing");
+//			climb = false;
+//		}
 
 		if(move > 0 && !right) {
 			Flip();
@@ -60,14 +82,5 @@ public class UrtleCode : MonoBehaviour {
 		transform.localScale = scale;
 	}
 		
-	void OnTriggerEnter2D(Collider2D other){
-
-		if(other.CompareTag("Puzzle Piece")){
-			Destroy(other.gameObject);
-			//GameManager.AddPoints (1);
-			//if (other.CompareTag ("Ladder")) {
-				//		}
-		}
-	}
 
 }
